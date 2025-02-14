2da6a11ebae5960958f276156f3894e69ae7ac8f
GabrielleDuda <gabiabacduda@gmail.com>
2025-02-14 01:46:01 -0300

    atualização das pastas

--- backend/api/db.php
+++ backend/api/db.php
@@ -2,8 +2,7 @@
 $host = "localhost";
 $dbname = "delicias_rita";
 $username = "root";
-$password = ""; // Adicione sua senha aqui, se houver
-
+$password = "00"; 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
